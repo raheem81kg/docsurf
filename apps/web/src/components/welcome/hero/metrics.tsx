0fe75f661ef34456643cea0682dcd5d30e037fdb
Raheem Gordon <raheemgor81@gmail.com>
2025-07-22 08:45:40 -0500

    chore: update landing page metrics

--- apps/web/src/components/welcome/hero/metrics.tsx
+++ apps/web/src/components/welcome/hero/metrics.tsx
@@ -3,7 +3,7 @@ export function Metrics() {
       <div className="bottom-0 left-0 mt-20 grid grid-cols-2 gap-8 md:flex md:flex-nowrap md:divide-x lg:absolute lg:mt-0">
          <div className="flex flex-col text-center md:pr-8">
             <h4 className="mb-4 text-[#878787] text-sm">Active Users</h4>
-            <span className="font-mono text-2xl text-stroke">350+</span>
+            <span className="font-mono text-2xl text-stroke">400+</span>
          </div>
          <div className="flex flex-col text-center md:px-8">
             <h4 className="mb-4 text-[#878787] text-sm">Documents Created</h4>
@@ -11,11 +11,11 @@ export function Metrics() {
          </div>
          <div className="flex flex-col text-center md:px-8">
             <h4 className="mb-4 text-[#878787] text-sm">AI Chat Messages</h4>
-            <span className="font-mono text-2xl text-stroke">10K+</span>
+            <span className="font-mono text-2xl text-stroke">6K+</span>
          </div>
          <div className="flex flex-col text-center md:px-8">
             <h4 className="mb-4 text-[#878787] text-sm">Words Written</h4>
-            <span className="font-mono text-2xl text-stroke">350K+</span>
+            <span className="font-mono text-2xl text-stroke">500K+</span>
          </div>
       </div>
    );
